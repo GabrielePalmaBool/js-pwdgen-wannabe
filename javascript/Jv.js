8dece02c0c6ee502a2d9e0ea573c69ec9b107a16
Gabriele <gabriele.palma.bool@gmail.com>
2023-11-16 15:42:03 +0100

    finish

---
+++ javascript/Jv.js
@@ -0,0 +1,12 @@
+// funzione che permette di inserire i dati e associare tali infomrazioni a delle variabili
+const nomeUser = prompt("inserisci il tuo nome");
+const cognomeUser = prompt("inserisci il tuo cognome");
+const colore = prompt("quale è il tuo colore preferito?");
+
+let = nomeUser, cognomeUser, colore;
+
+console.log(let);
+
+// Stampo il risultato in pagina
+
+document.getElementById("risultato").innerHTML = "La tu password sarà: " +nomeUser +cognomeUser +colore  + "23";
